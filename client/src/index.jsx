64e30366bd9ca662021a1844b5417c1e4eb0e77e
code8la888 <kazuhaha1997@gmail.com>
2025-01-22 21:29:17 +0800

    feat: add ToastContainer

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,7 +6,8 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 import { thunk } from "redux-thunk";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
@@ -15,5 +16,17 @@ const store = createStore(reducers, {}, applyMiddleware(thunk));
 root.render(
   <Provider store={store}>
     <App />
+    <ToastContainer
+      position="top-center"
+      autoClose={1000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
   </Provider>
 );
