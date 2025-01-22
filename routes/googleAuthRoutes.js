c427a3367e023dcee0e7c356cf99ebe39bcae142
code8la888 <kazuhaha1997@gmail.com>
2025-01-22 21:44:54 +0800

    feat: modify redirect path in googleAuthRoutes

--- routes/googleAuthRoutes.js
+++ routes/googleAuthRoutes.js
@@ -25,7 +25,7 @@ router.get("/auth/google/callback", (req, res, next) => {
       if (err) {
         return next(err);
       }
-      res.redirect("/index");
+      res.redirect("/strains/index");
     });
   })(req, res, next);
 });
