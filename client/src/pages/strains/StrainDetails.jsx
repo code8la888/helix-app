c17b6e979d2864eaf6e7b7e8ed72b83d0f03e26b
code8la888 <kazuhaha1997@gmail.com>
2025-01-23 16:48:05 +0800

    feat(StrainDetails): modify styles and add pagination function

--- client/src/pages/strains/StrainDetails.jsx
+++ client/src/pages/strains/StrainDetails.jsx
@@ -7,6 +7,7 @@ import { fetchUser } from "../../actions/index";
 import { sendFormData } from "../../utils/sendFormData";
 import { fetchData } from "../../utils/fetchData";
 import { toast } from "react-toastify";
+import ReactPaginate from "react-paginate";
 
 export default function StrainDetails() {
   const { id } = useParams();
@@ -18,6 +19,9 @@ export default function StrainDetails() {
   const currentUser = useSelector((state) => state.auth);
   const navigate = useNavigate();
   const [isAuthorized, setIsAuthorized] = useState(null);
+  const itemsPerPage = 10;
+  const [mouseOffset, setMouseOffset] = useState(0);
+  const [breedingOffset, setBreedingOffset] = useState(0);
   const loadData = async () => {
     try {
       const res = await fetchData(`/api/strains/${id}`);
@@ -52,6 +56,21 @@ export default function StrainDetails() {
     fetchPermission();
   }, [dispatch, navigate, id]);
 
+  const paginate = (items, offset, itemsPerPage) => {
+    const endOffset = offset + itemsPerPage;
+    return items.slice(offset, endOffset);
+  };
+
+  const getPageCount = (items) => Math.ceil(items.length / itemsPerPage);
+
+  const paginatedMice = paginate(mice, mouseOffset, itemsPerPage);
+
+  const paginatedBreedingRecords = paginate(
+    breedingRecord,
+    breedingOffset,
+    itemsPerPage
+  );
+
   const handleDeleteStrain = async (event) => {
     event.preventDefault();
     await sendFormData(`/api/strains/${id}`, undefined, navigate, "DELETE");
@@ -84,14 +103,14 @@ export default function StrainDetails() {
   };
 
   return (
-    <div>
+    <>
       <h1>NTUMC-LAC 基因改造小鼠採樣記錄</h1>
-      <br />
+
       {currentUser &&
       strain?.users?.includes(currentUser.username) &&
       currentUser.role === "品系管理人" ? (
         <div className="mb-3 d-flex">
-          <h3>品系資訊:</h3>
+          <h3>品系資訊</h3>
           <button className="btn btn-warning text-white mx-2">
             <Link
               to={`/strains/${id}/edit`}
@@ -109,12 +128,12 @@ export default function StrainDetails() {
         ""
       )}
       {strain ? (
-        <div className="row g-3 mouse.-2">
-          <div className="col-mouse.-3">品系名稱: {strain.strain}</div>
-          <div className="col-mouse.-2">品系簡稱: {strain.abbr}</div>
-          <div className="col-mouse.-2">單位: {strain.dept}</div>
-          <div className="col-mouse.-2">IACUC編號: {strain.iacuc_no}</div>
-          <div className="col-mouse.-2">
+        <div className="row g-3 my-3">
+          <div>品系名稱: {strain.strain}</div>
+          <div>品系簡稱: {strain.abbr}</div>
+          <div>單位: {strain.dept}</div>
+          <div>IACUC編號: {strain.iacuc_no}</div>
+          <div>
             計畫期限: {new Date(strain.EXP).toLocaleDateString("zh-TW")}
           </div>
         </div>
@@ -122,33 +141,35 @@ export default function StrainDetails() {
         ""
       )}
       <br />
-      <h3>使用者資訊:</h3>
-      <table className="table table-striped mouse.-2">
-        <thead>
-          <tr>
-            <th scope="col">姓名:</th>
-            <th scope="col">職稱:</th>
-            <th scope="col">聯絡信箱:</th>
-            <th scope="col">聯絡電話:</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users
-            ? users
-                .filter((user) => user.role !== "獸醫")
-                .map((user) => (
-                  <tr key={user._id}>
-                    <td>{user.username}</td>
-                    <td>{user.role}</td>
-                    <td>{user.email}</td>
-                    <td>{user.tel}</td>
-                  </tr>
-                ))
-            : "loading"}
-        </tbody>
-      </table>
-      <br />
-      <h3>小鼠採樣記錄:</h3>
+      <h3>使用者資訊</h3>
+      <div className="shadow-lg mt-3 mb-5 p-3 rounded-3">
+        <table className="table table-striped table-hover custom-striped">
+          <thead>
+            <tr>
+              <th scope="col">姓名:</th>
+              <th scope="col">職稱:</th>
+              <th scope="col">聯絡信箱:</th>
+              <th scope="col">聯絡電話:</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users
+              ? users
+                  .filter((user) => user.role !== "獸醫")
+                  .map((user) => (
+                    <tr key={user._id}>
+                      <td>{user.username}</td>
+                      <td>{user.role}</td>
+                      <td>{user.email}</td>
+                      <td>{user.tel}</td>
+                    </tr>
+                  ))
+              : "loading"}
+          </tbody>
+        </table>
+      </div>
+
+      <h3>小鼠採樣記錄</h3>
       {currentUser &&
       strain?.users?.includes(currentUser.username) &&
       currentUser.role === "品系管理人" ? (
@@ -156,94 +177,118 @@ export default function StrainDetails() {
       ) : (
         ""
       )}
-      <table className="table table-striped mouse.-2">
-        <thead>
-          <tr>
-            <th scope="col">No</th>
-            <th scope="col">父母</th>
-            <th scope="col">胎次</th>
-            <th scope="col">性別</th>
-            <th scope="col">出生日期</th>
-            <th scope="col">採樣日期</th>
-            <th scope="col">趾號</th>
-            {strain
-              ? strain.genes.map((gene) => (
-                  <th scope="col" key={gene}>
-                    {gene}
-                  </th>
-                ))
-              : ""}
-            <th scope="col">狀態</th>
-            <th scope="col">備註</th>
-            {currentUser &&
-            strain?.users?.includes(currentUser.username) &&
-            currentUser.role === "品系管理人" ? (
-              <>
-                <th scope="col">編輯</th>
-                <th scope="col">刪除</th>
-              </>
-            ) : (
-              ""
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          {mice.map((mouse) => (
-            <tr key={mouse._id}>
-              <td scope="row">{mouse.no}</td>
-              <td>
-                {mouse.parents.father} x {mouse.parents.mother}
-              </td>
-              <td>{mouse.litter}</td>
-              <td>{mouse.gender}</td>
-              <td>{new Date(mouse.birth_date).toLocaleDateString("zh-TW")}</td>
-              <td>
-                {new Date(mouse.sampling_date).toLocaleDateString("zh-TW")}
-              </td>
-              <td>{mouse.toeNumber}</td>
-              {mouse?.sampling_results?.length > 0 ? (
-                mouse.sampling_results.map((result, index) => (
-                  <td key={index}>{result}</td>
-                ))
-              ) : (
-                <td value="檢測中">檢測中</td>
-              )}
-              <td>{mouse.on_shelf}</td>
-              <td>{mouse.note ? mouse.note : "-"}</td>
+      <div className="shadow-lg mt-3 mb-5 p-3 rounded-3">
+        <table className="table table-striped table-hover custom-striped">
+          <thead>
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">父母</th>
+              <th scope="col">胎次</th>
+              <th scope="col">性別</th>
+              <th scope="col">出生日期</th>
+              <th scope="col">採樣日期</th>
+              <th scope="col">趾號</th>
+              {strain
+                ? strain.genes.map((gene) => (
+                    <th scope="col" key={gene}>
+                      {gene}
+                    </th>
+                  ))
+                : ""}
+              <th scope="col">狀態</th>
+              <th scope="col">備註</th>
               {currentUser &&
               strain?.users?.includes(currentUser.username) &&
               currentUser.role === "品系管理人" ? (
                 <>
-                  <td>
-                    <button className="btn btn-warning">
-                      <Link
-                        to={`/strains/${id}/mice/${mouse._id}/edit`}
-                        style={{ textDecoration: "none", color: "black" }}
-                      >
-                        編輯
-                      </Link>
-                    </button>
-                  </td>
-                  <td>
-                    <button
-                      className="btn btn-danger"
-                      onClick={(event) => {
-                        handleDeleteMice(mouse._id, event);
-                      }}
-                    >
-                      刪除
-                    </button>
-                  </td>
+                  <th scope="col">編輯</th>
+                  <th scope="col">刪除</th>
                 </>
               ) : (
                 ""
               )}
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <br />
-      <h3>繁殖記錄表:</h3>
+          </thead>
+          <tbody>
+            {paginatedMice.map((mouse) => (
+              <tr key={mouse._id}>
+                <td>{mouse.no}</td>
+                <td>
+                  {mouse.parents.father} x {mouse.parents.mother}
+                </td>
+                <td>{mouse.litter}</td>
+                <td>{mouse.gender}</td>
+                <td>
+                  {new Date(mouse.birth_date).toLocaleDateString("zh-TW")}
+                </td>
+                <td>
+                  {new Date(mouse.sampling_date).toLocaleDateString("zh-TW")}
+                </td>
+                <td>{mouse.toeNumber}</td>
+                {mouse?.sampling_results?.length > 0 ? (
+                  mouse.sampling_results.map((result, index) => (
+                    <td key={index}>{result}</td>
+                  ))
+                ) : (
+                  <td value="檢測中">檢測中</td>
+                )}
+                <td>{mouse.on_shelf}</td>
+                <td>{mouse.note || "-"}</td>
+                {currentUser &&
+                strain?.users?.includes(currentUser.username) &&
+                currentUser.role === "品系管理人" ? (
+                  <>
+                    <td>
+                      <button className="btn btn-warning">
+                        <Link
+                          to={`/strains/${id}/mice/${mouse._id}/edit`}
+                          style={{ textDecoration: "none", color: "black" }}
+                        >
+                          編輯
+                        </Link>
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={(event) => {
+                          handleDeleteMice(mouse._id, event);
+                        }}
+                      >
+                        刪除
+                      </button>
+                    </td>
+                  </>
+                ) : (
+                  ""
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="下一頁 >"
+          previousLabel="< 上一頁"
+          onPageChange={(event) =>
+            setMouseOffset(event.selected * itemsPerPage)
+          }
+          pageCount={getPageCount(mice)}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          containerClassName="pagination justify-content-center"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          activeClassName="active"
+        />
+      </div>
+
+      <h3>繁殖記錄表</h3>
       {currentUser &&
       strain?.users.includes(currentUser.username) &&
       currentUser.role === "品系管理人" ? (
@@ -251,70 +296,89 @@ export default function StrainDetails() {
       ) : (
         ""
       )}
-      <table className="table table-striped mouse.-2">
-        <thead>
-          <tr>
-            <th scope="col">繁殖籠編號</th>
-            <th scope="col">父母</th>
-            <th scope="col">配種日期</th>
-            <th scope="col">繁殖籠狀態</th>
-            {currentUser &&
-            strain?.users?.includes(currentUser.username) &&
-            currentUser.role === "品系管理人" ? (
-              <>
-                <th scope="col">編輯</th>
-                <th scope="col">刪除</th>
-              </>
-            ) : (
-              ""
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          {breedingRecord
-            ? breedingRecord.map((b) => (
-                <tr key={b._id}>
-                  <th scope="row">{b.cage_no}</th>
-                  <td>
-                    {b.parents.father} 配 {b.parents.mother}
-                  </td>
-                  <td>
-                    {new Date(b.pairing_date).toLocaleDateString("zh-TW")}
-                  </td>
-                  <td>{b.on_shelf}</td>
-                  {currentUser &&
-                  strain?.users?.includes(currentUser.username) &&
-                  currentUser.role === "品系管理人" ? (
-                    <>
-                      <td>
-                        <button className="btn btn-warning">
-                          <Link
-                            to={`/strains/${id}/breedingRecord/${b._id}/edit`}
-                            style={{ textDecoration: "none", color: "black" }}
-                          >
-                            編輯
-                          </Link>
-                        </button>
-                      </td>
-                      <td>
-                        <button
-                          className="btn btn-danger"
-                          onClick={(event) => {
-                            handleDeleteBreedingRecord(b._id, event);
-                          }}
+      <div className="shadow-lg mt-3 mb-5 p-3 rounded-3">
+        <table className="table table-striped table-hover custom-striped">
+          <thead>
+            <tr>
+              <th scope="col">繁殖籠編號</th>
+              <th scope="col">父母</th>
+              <th scope="col">配種日期</th>
+              <th scope="col">繁殖籠狀態</th>
+              {currentUser &&
+              strain?.users?.includes(currentUser.username) &&
+              currentUser.role === "品系管理人" ? (
+                <>
+                  <th scope="col">編輯</th>
+                  <th scope="col">刪除</th>
+                </>
+              ) : (
+                ""
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {paginatedBreedingRecords.map((record) => (
+              <tr key={record._id}>
+                <td>{record.cage_no}</td>
+                <td>
+                  {record.parents.father} 配 {record.parents.mother}
+                </td>
+                <td>
+                  {new Date(record.pairing_date).toLocaleDateString("zh-TW")}
+                </td>
+                <td>{record.on_shelf}</td>
+                {currentUser &&
+                strain?.users?.includes(currentUser.username) &&
+                currentUser.role === "品系管理人" ? (
+                  <>
+                    <td>
+                      <button className="btn btn-warning">
+                        <Link
+                          to={`/strains/${id}/breedingRecord/${record._id}/edit`}
+                          style={{ textDecoration: "none", color: "black" }}
                         >
-                          刪除
-                        </button>
-                      </td>
-                    </>
-                  ) : (
-                    ""
-                  )}
-                </tr>
-              ))
-            : ""}
-        </tbody>
-      </table>
-    </div>
+                          編輯
+                        </Link>
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={(event) => {
+                          handleDeleteBreedingRecord(record._id, event);
+                        }}
+                      >
+                        刪除
+                      </button>
+                    </td>
+                  </>
+                ) : (
+                  ""
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="下一頁 >"
+          previousLabel="< 上一頁"
+          onPageChange={(event) =>
+            setBreedingOffset(event.selected * itemsPerPage)
+          }
+          pageCount={getPageCount(breedingRecord)}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          containerClassName="pagination justify-content-center"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          activeClassName="active"
+        />
+      </div>
+    </>
   );
 }
