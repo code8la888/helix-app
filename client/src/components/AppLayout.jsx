3d609e30309a5a3698cf54812734ad4da8b22d84
code8la888 <kazuhaha1997@gmail.com>
2025-01-25 23:17:52 +0800

    feat: add scroll up buttom

--- client/src/components/AppLayout.jsx
+++ client/src/components/AppLayout.jsx
@@ -1,18 +1,27 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Container from "react-bootstrap/Container";
+import ScrollToTop from "react-scroll-up";
+import { FaChevronCircleUp } from "react-icons/fa";
 
 function AppLayout({ children }) {
   return (
-    <div
-      className="d-flex flex-column vh-100"
-      // style={{ backgroundColor: "rgb(139, 185, 254)" }}
-    >
+    <div className="d-flex flex-column vh-100">
       <Header />
       <main>
         <Container className="mt-3 mb-3">{children}</Container>
       </main>
       <Footer />
+      <ScrollToTop showUnder={150}>
+        <span>
+          <FaChevronCircleUp
+            style={{
+              fontSize: "40",
+              color: "rgb(139, 185, 254)",
+            }}
+          />
+        </span>
+      </ScrollToTop>
     </div>
   );
 }
