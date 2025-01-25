74aff4320f00752c520e4cfdb992e985e53d8f9a
code8la888 <kazuhaha1997@gmail.com>
2025-01-26 01:34:38 +0800

    feat: add lazy loading

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,41 @@
-import { Component } from "react";
+import { Component, lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
-import ProtectedRoute from "./ProtectedRoute";
-import AppLayout from "./AppLayout";
-import LoginPage from "../pages/LoginPage";
-import Landing from "./Landing";
-import RegisterPage from "../pages/RegisterPage";
-import Index from "../pages/strains/Index";
-import NewStrain from "../pages/strains/NewStrain";
-import StrainDetails from "../pages/strains/StrainDetails";
-import EditStrain from "../pages/strains/EditStrain";
-import NewMice from "../pages/mice/NewMice";
-import NewBreedingRecord from "../pages/breedingRecords/NewBreedingRecord";
-import EditBreedingRecord from "../pages/breedingRecords/EditBreedingRecord";
-import EditMice from "../pages/mice/EditMice";
-import ErrorPage from "../pages/ErrorPage";
+// import ProtectedRoute from "./ProtectedRoute";
+// import AppLayout from "./AppLayout";
+// import LoginPage from "../pages/LoginPage";
+// import Landing from "./Landing";
+// import RegisterPage from "../pages/RegisterPage";
+// import Index from "../pages/strains/Index";
+// import NewStrain from "../pages/strains/NewStrain";
+// import StrainDetails from "../pages/strains/StrainDetails";
+// import EditStrain from "../pages/strains/EditStrain";
+// import NewMice from "../pages/mice/NewMice";
+// import NewBreedingRecord from "../pages/breedingRecords/NewBreedingRecord";
+// import EditBreedingRecord from "../pages/breedingRecords/EditBreedingRecord";
+// import EditMice from "../pages/mice/EditMice";
+// import ErrorPage from "../pages/ErrorPage";
+
+const ProtectedRoute = lazy(() => import("./ProtectedRoute"));
+const AppLayout = lazy(() => import("./AppLayout"));
+const LoginPage = lazy(() => import("../pages/LoginPage"));
+const Landing = lazy(() => import("./Landing"));
+const RegisterPage = lazy(() => import("../pages/RegisterPage"));
+const Index = lazy(() => import("../pages/strains/Index"));
+const NewStrain = lazy(() => import("../pages/strains/NewStrain"));
+const StrainDetails = lazy(() => import("../pages/strains/StrainDetails"));
+const EditStrain = lazy(() => import("../pages/strains/EditStrain"));
+const NewMice = lazy(() => import("../pages/mice/NewMice"));
+const NewBreedingRecord = lazy(() =>
+  import("../pages/breedingRecords/NewBreedingRecord")
+);
+const EditBreedingRecord = lazy(() =>
+  import("../pages/breedingRecords/EditBreedingRecord")
+);
+const EditMice = lazy(() => import("../pages/mice/EditMice"));
+const ErrorPage = lazy(() => import("../pages/ErrorPage"));
 
 class App extends Component {
   componentDidMount() {
@@ -28,41 +47,49 @@ class App extends Component {
 
     return (
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+        <Suspense fallback={<p>載入中...</p>}>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
 
-          <Route
-            path="*"
-            element={
-              <ProtectedRoute isLoggedIn={isLoggedIn}>
-                <AppLayout>
-                  <Routes>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="/strains/index" element={<Index />} />
-                    <Route path="/strain/new" element={<NewStrain />} />
-                    <Route path="/strains/:id" element={<StrainDetails />} />
-                    <Route path="/strains/:id/edit" element={<EditStrain />} />
-                    <Route path="/strains/:id/mice/new" element={<NewMice />} />
-                    <Route
-                      path="/strains/:strainId/breedingRecord/:breedingRecordId/edit"
-                      element={<EditBreedingRecord />}
-                    />
-                    <Route
-                      path="/strains/:strainId/mice/:mouseId/edit"
-                      element={<EditMice />}
-                    />
-                    <Route
-                      path="/strains/:id/breedingRecord/new"
-                      element={<NewBreedingRecord />}
-                    />
-                    <Route path="/error" element={<ErrorPage />} />
-                  </Routes>
-                </AppLayout>
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+            <Route
+              path="*"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn}>
+                  <AppLayout>
+                    <Routes>
+                      <Route path="/" element={<Landing />} />
+                      <Route path="/strains/index" element={<Index />} />
+                      <Route path="/strain/new" element={<NewStrain />} />
+                      <Route path="/strains/:id" element={<StrainDetails />} />
+                      <Route
+                        path="/strains/:id/edit"
+                        element={<EditStrain />}
+                      />
+                      <Route
+                        path="/strains/:id/mice/new"
+                        element={<NewMice />}
+                      />
+                      <Route
+                        path="/strains/:strainId/breedingRecord/:breedingRecordId/edit"
+                        element={<EditBreedingRecord />}
+                      />
+                      <Route
+                        path="/strains/:strainId/mice/:mouseId/edit"
+                        element={<EditMice />}
+                      />
+                      <Route
+                        path="/strains/:id/breedingRecord/new"
+                        element={<NewBreedingRecord />}
+                      />
+                      <Route path="/error" element={<ErrorPage />} />
+                    </Routes>
+                  </AppLayout>
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     );
   }
