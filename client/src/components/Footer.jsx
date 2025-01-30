838658266a2dae858fd4eefad2e2f7a9c471df36
code8la888 <kazuhaha1997@gmail.com>
2025-01-30 23:07:31 +0800

    chore: remove unused component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,12 @@
-import Container from "react-bootstrap/Container";
 function Footer() {
   return (
     <footer
       className="footer py-3 mt-auto"
       style={{ backgroundColor: "rgb(6, 60, 139)" }}
     >
-      <Container className="text-center wh">
+      <div className="container text-center wh">
         <span>&copy; Helix LIMS 2024</span>
-      </Container>
+      </div>
     </footer>
   );
 }
