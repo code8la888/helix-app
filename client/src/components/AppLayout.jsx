8c572eadde1398e1ddc65eadc2493ba91db12aa9
code8la888 <kazuhaha1997@gmail.com>
2025-01-30 23:05:15 +0800

    fix: Fixed incorrect usage of Outlet in AppLayout.jsx

--- client/src/components/AppLayout.jsx
+++ client/src/components/AppLayout.jsx
@@ -1,22 +1,24 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import Container from "react-bootstrap/Container";
+import { Outlet } from "react-router-dom";
 import ScrollToTop from "react-scroll-up";
 import { FaChevronCircleUp } from "react-icons/fa";
 
-function AppLayout({ children }) {
+function AppLayout() {
   return (
     <div className="d-flex flex-column vh-100">
       <Header />
-      <main>
-        <Container className="mt-3 mb-3">{children}</Container>
+      <main className="flex-grow-1">
+        <div className="mt-3 mb-3 container">
+          <Outlet />
+        </div>
       </main>
       <Footer />
       <ScrollToTop showUnder={150}>
         <span>
           <FaChevronCircleUp
             style={{
-              fontSize: "40",
+              fontSize: 40,
               color: "rgb(139, 185, 254)",
             }}
           />
