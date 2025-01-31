daf339d892223ee254a3438ccc921e8b763b0be0
code8la888 <kazuhaha1997@gmail.com>
2025-02-01 00:02:47 +0800

    feat: modify dashboard content

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <section className="container-fluid p-5">
+      <div className="row row-cols-1 row-cols-md-2 g-4">
+        {[
+          { title: "個人資訊", href: "/profile", icon: "👤" },
+          { title: "數據平台", href: "/strains/index", icon: "📊" },
+        ].map((feature, index) => (
+          <div key={index} className="col">
+            <div
+              className="card text-center h-100"
+              style={{ boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)" }}
+            >
+              <div className="card-body">
+                <Link
+                  to={feature.href}
+                  style={{ textDecoration: "none", color: "rgb(6, 60, 139)" }}
+                >
+                  <h1>{feature.icon}</h1>
+                  <h5 className="card-title text-black fw-bold">
+                    {feature.title}
+                  </h5>
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
