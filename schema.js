da2bdc0f58d8508af776fd960eee91d1f85915c3
code8la888 <kazuhaha1997@gmail.com>
2025-02-02 20:06:52 +0800

    feat: modify: error message

--- schema.js
+++ schema.js
@@ -5,7 +5,7 @@ const extension = (joi) => ({
   type: "string",
   base: joi.string(),
   messages: {
-    "string.escapeHTML": "{{#label}} must not include HTML!",
+    "string.escapeHTML": "{{#label}} 不能包含HTML內容!",
   },
   rules: {
     escapeHTML: {
