3a7db5566adb3e98a0188ff6b08aaa33db5214dc
code8la888 <kazuhaha1997@gmail.com>
2025-02-04 16:57:53 +0800

    refactor(Header): Retrieve user state from Redux Store and convert Class Component to Function Component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,77 +1,79 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { SiHelix } from "react-icons/si";
+import { useEffect } from "react";
+import { fetchUser } from "../actions/authActions";
 
-class Header extends Component {
-  renderContent() {
-    switch (this.props.auth) {
-      case null:
-        return;
-      case false:
-        return (
-          <>
-            <li className="nav-item">
-              <Link className="nav-link wh" to="/login">
-                登入
-              </Link>
-            </li>
-            <li className="nav-item ">
-              <Link className="nav-link wh" to="/register">
-                註冊
-              </Link>
-            </li>
-          </>
-        );
-      default:
-        return (
-          <>
-            <li className="nav-item">
-              <a className="nav-link wh" href="/api/logout">
-                登出
-              </a>
-            </li>
-          </>
-        );
+export default function Header() {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  const renderContent = () => {
+    if (auth === null) {
+      return;
     }
-  }
+    if (auth === false) {
+      return (
+        <>
+          <li className="nav-item">
+            <Link className="nav-link wh" to="/login">
+              登入
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link wh" to="/register">
+              註冊
+            </Link>
+          </li>
+        </>
+      );
+    }
 
-  render() {
     return (
-      <nav
-        className="navbar navbar-expand-lg sticky-top"
-        style={{ backgroundColor: "rgb(6, 60, 139)" }}
-      >
-        <div className="container">
-          <Link
-            className="navbar-brand wh"
-            to={this.props.user ? "/strains/index" : "/home"}
-          >
-            <SiHelix />
-            Helix
+      <>
+        <li>
+          <Link className="nav-link wh" to="/dashboard">
+            我的儀錶板
           </Link>
-          <button
-            className="navbar-toggler custom-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">{this.renderContent()}</ul>
-          </div>
-        </div>
-      </nav>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link wh" href="/api/logout">
+            登出
+          </a>
+        </li>
+      </>
     );
-  }
-}
+  };
 
-function mapStateToProps({ auth }) {
-  return { auth };
+  return (
+    <nav
+      className="navbar navbar-expand-lg sticky-top"
+      style={{ backgroundColor: "rgb(6, 60, 139)" }}
+    >
+      <div className="container">
+        <Link className="navbar-brand wh" to={auth ? "/dashboard" : "/home"}>
+          <SiHelix />
+          Helix
+        </Link>
+        <button
+          className="navbar-toggler custom-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">{renderContent()}</ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
-
-export default connect(mapStateToProps)(Header);
